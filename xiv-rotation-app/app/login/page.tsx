dc21a003c5549f3ccdda43dad894e8842707bc45
akino_harusaki <kindan80road2@gmail.com>
2024-04-09 02:31:12 +0900

    add loading components

--- xiv-rotation-app/app/login/page.tsx
+++ xiv-rotation-app/app/login/page.tsx
@@ -1,92 +1,31 @@
-import Link from "next/link";
-import { headers } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+'use client';
+import { BackLinkButton } from "@/components/BackLinkButton";
+import { Loading } from "@/components/Loading";
+import { useTransition } from "react";
+import { signIn, signUp } from "./actions/login";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { message: string };
 }) {
-  /**
-   * check sign in 
-   * @param {FormData} formData 
-   * @returns
-   */
-  const signIn = async (formData: FormData) => {
-    "use server";
-
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
+  const [isPending, startTransition] = useTransition();
+  const onClickSignIn = async (formData: FormData) => {
+    startTransition(async () => {
+      await signIn(formData);
     });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/");
-  };
-
-  /**
-   * check sign up
-   * @param {FormData} formData 
-   * @returns 
-   */
-  const signUp = async (formData: FormData) => {
-    "use server";
-  
-    const origin = headers().get("origin");
-    const name = formData.get("")
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-    
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
-  };
-
+  }
+  const onClickSignUp = (formData: FormData) => {
+    startTransition(async () => {
+      await signUp(formData);
+    })
+  }
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
-
+      <BackLinkButton href="/"/>
       <form
         className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action={signIn}
+        action={onClickSignIn}
       >
         <label className="text-md" htmlFor="email">
           Email
@@ -107,19 +46,23 @@ export default function Login({
           placeholder="••••••••"
           required
         />
-        <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-white hover:text-green-700 active:scale-95">
-          Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-gray-700 active:scale-95"
-        >
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
+        {isPending ? <Loading /> : (
+          <>
+            <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-white hover:text-green-700 active:scale-95">
+              Sign In
+            </button>
+            <button
+              formAction={onClickSignUp}
+              className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2 hover:bg-gray-700 active:scale-95"
+            >
+              Sign Up
+            </button>
+            {searchParams?.message && (
+              <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                {searchParams.message}
+              </p>
+            )}
+          </>
         )}
       </form>
     </div>
--- xiv-rotation-app/app/setting/page.tsx
+++ xiv-rotation-app/app/setting/page.tsx
@@ -1,8 +1,6 @@
 'use client';
-import Link from "next/link";
 import { createClient } from "@/utils/supabase/client";
-import { redirect } from "next/navigation";
-
+import { BackLinkButton } from "@/components/BackLinkButton";
 export default function Login({
   searchParams,
 }: {
@@ -10,47 +8,23 @@ export default function Login({
 }) {
 
   const updateUserInfo = async (id: string | undefined, newData: object) => {
-    const response = await fetch('api/userSetting', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id,
-        newData
-      })
-    });
+    try {
+      const supabase = createClient();
+      const { error } = await supabase.from('profiles').update(newData).eq('id', id);
+    } catch(error) {
+
+    }
   };
   const changeName = async (formData: FormData) => {
     const supabase = createClient();
     const { data: { user } } = await supabase.auth.getUser();
 
-    updateUserInfo(user?.id, {username: formData.get('userName')});
+    updateUserInfo(user?.id, { username: formData.get('userName') });
   };
 
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
-
+      <BackLinkButton href="/" />
       <form
         className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
         action={changeName}
